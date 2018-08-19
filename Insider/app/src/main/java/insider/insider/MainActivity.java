e5d93755e5b2c86d951f306b74ce6941bb3e986e
Lim Hyun (임현) <hyunzion@gmail.com>
2018-08-19 20:51:49 +0900

    add MainActivity.java

--- Insider/app/src/main/java/insider/insider/MainActivity.java
+++ Insider/app/src/main/java/insider/insider/MainActivity.java
@@ -2,6 +2,11 @@ package insider.insider;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.Socket;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,61 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // 서버 아이피
+        String serverIp = "192.168.0.1";
+        String name = "Lim";
+
+        // 소켓 연결
+        try {
+            Socket socket = new Socket(serverIp, 11054);
+            Log.d("socket", "서버 연결");
+            Thread sender = new Thread(new ClientSender(socket, name));
+            Thread receiver = new Thread(new ClientReceiver(socket));
+            sender.start();
+            receiver.start();
+        } catch (Exception e) {
+        }
+    }
+
+    static class ClientSender extends Thread {
+        Socket socket;
+        DataOutputStream out;
+        String name;
+
+        ClientSender(Socket socket, String name) {
+            this.socket = socket;
+            try {
+                out = new DataOutputStream(socket.getOutputStream());
+                this.name = name;
+            } catch (Exception e) {
+            }
+        }
+
+        public void run() {
+            // 입력
+        }
+    }
+
+    static class ClientReceiver extends Thread {
+        Socket socket;
+        DataInputStream in;
+
+        ClientReceiver(Socket socket) {
+            this.socket = socket;
+            try {
+                in = new DataInputStream(socket.getInputStream());
+            } catch (Exception e) {
+            }
+        }
+
+        public void run() {
+            while (in != null) {
+                try {
+                    // 출력
+                } catch (Exception e) {
+                }
+            }
+        }
     }
 }
